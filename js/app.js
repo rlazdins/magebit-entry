2eacb1f8d4fd17c0faec094286a033df621d68be
rlazdins <renars.lazdins@gmail.com>
2021-03-27 22:10:47 +0200

    initial with php

--- js/app.js
+++ js/app.js
@@ -1,55 +1,68 @@
-
-let emailEl,agreedTermsEl;
-let canSubscribe = emailValid = agreedTerms = false;
+let emailEl, agreedTermsEl;
+let canSubscribe = (emailValid = agreedTerms = false);
 let form;
-
 
 // kad dokuments ir ielādējies
 document.onreadystatechange = () => {
-    if(document.readyState == "interactive"){
-        emailEl=document.getElementById("e-mail")
-        agreedTermsEl=document.getElementById("checkbox") 
-        form = document.getElementById("subscribe")
+	if (document.readyState == "interactive") {
+		emailEl = document.getElementById("e-mail");
+		agreedTermsEl = document.getElementById("checkbox");
+		form = document.getElementById("subscribe");
 
-        emailEl.addEventListener("blur",function(){
-            emailValid = validateEmail(this.value);
-            validateSubscribe();
+		emailEl.addEventListener("blur", function () {
+			emailValid = validateEmail(this.value);
+			validateSubscribe();
 
-            if(emailValid){
-                document.getElementById("email-status").classList.add("hidden");
-            }else{
-                document.getElementById("email-status").classList.remove("hidden");
-            }
-        })
+			if (emailValid) {
+				document.getElementById("email-status").classList.add("hidden");
+			} else {
+				document.getElementById("email-status").classList.remove("hidden");
+			}
+		});
 
-        agreedTermsEl.addEventListener("click", function() {
-            agreedTerms = this.checked;
-            validateSubscribe();
-        })
+		agreedTermsEl.addEventListener("click", function () {
+			agreedTerms = this.checked;
+			validateSubscribe();
+		});
 
-        form.addEventListener("submit", function(e){
-            e.preventDefault();
+		form.addEventListener("submit", function (e) {
+			e.preventDefault();
 
-            if(agreedTerms){
-                document.getElementById("terms-status").classList.add("hidden");
-            }else{
-                document.getElementById("terms-status").classList.remove("hidden");
-            }
+			if (agreedTerms) {
+				document.getElementById("terms-status").classList.add("hidden");
+			} else {
+				document.getElementById("terms-status").classList.remove("hidden");
+			}
 
-            if(canSubscribe) {
-                document.getElementById("intro").classList.add("hidden");
-                document.getElementById("outro").classList.remove("hidden");
-                document.getElementById("subscribe").classList.add("hidden");
-            }
-        })
-    }
-}
+			if (canSubscribe) {
+				// document.getElementById("intro").classList.add("hidden");
+				// document.getElementById("outro").classList.remove("hidden");
+				// document.getElementById("subscribe").classList.add("hidden");
+
+                data = "email=" + emailEl.value;
+
+                var xhr = new XMLHttpRequest();
+                var url = "php/subscribe.php";
+                xhr.open('POST', url, true);
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+                xhr.send(data);
+            
+                xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                      console.log(xhr.responseText);
+                    }
+                };
+
+			}
+		});
+	}
+};
 
 function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
 
 function validateSubscribe() {
-    canSubscribe = emailValid && agreedTerms;
+	canSubscribe = emailValid && agreedTerms;
 }
--- php/subscribe.php
+++ php/subscribe.php
@@ -7,3 +7,30 @@ require_once "config.php";
 $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 check_mysql($db);
 
+if(isset($_POST) && isset($_POST['email'])){
+    $email = $_POST['email'];
+
+    $query = $db->query("SELECT email FROM subscriptions WHERE email = '" . $email . "'");
+
+    if($query->num_rows == 0) {
+        $blacklist = file('../data/blacklist.txt', FILE_IGNORE_NEW_LINES);
+        $email_domain = explode('@', $email);
+        $email_domain = $email_domain[1];
+
+        if(in_array($email_domain, $blacklist)){
+            echo json_encode("blacklist");
+        }else{
+            if ($db->query("INSERT INTO subscriptions (email) VALUES ('" . $email . "')")) {
+                echo json_encode("New record created successfully");
+              } else {
+                echo json_encode("Error: " . $sql . "<br>" . $conn->error);
+              }
+            echo json_encode("added");
+        }
+
+    }else{
+        echo json_encode("exists");
+    }
+
+    $db->close();
+}
